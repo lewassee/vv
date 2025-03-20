f0c0efa8649a864e470be369a5278face3992b37
lewassee <93545973+lewassee@users.noreply.github.com>
2025-03-21 02:04:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Плавная прокрутка для всех ссылок с якорями
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
+  
